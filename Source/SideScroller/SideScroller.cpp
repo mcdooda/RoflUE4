8c2038dbef90dbd4a75c5cd73fc26559ddbb052b
mcdooda <nicolas.dod@gmail.com>
2016-03-02 22:03:46 +0100

    add source

---
+++ Source/SideScroller/SideScroller.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SideScroller.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SideScroller, "SideScroller" );
